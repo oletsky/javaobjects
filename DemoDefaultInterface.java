8f3daa34cd866e005dcf0d0ef14574b54fc4260a
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-02 19:28:27 +0300

    Add files via upload

---
+++ DemoDefaultInterface.java
@@ -0,0 +1,37 @@
+package javatraining.oletsky.oop;
+
+public class DemoDefaultInterface {
+    public static void main(String[] args) {
+        WithDefaults ekz = new DisObeyed();
+        ekz.ordinaryFoo();
+        ekz.defaultFoo1();
+        ekz.defaultFoo2();
+    }
+}
+
+interface WithDefaults {
+    void ordinaryFoo();
+
+    default void defaultFoo1() {
+        System.out.println("This must be done!!");
+    }
+
+    default void defaultFoo2() {
+        System.out.println("This should be done to");
+    }
+}
+
+class DisObeyed implements WithDefaults {
+
+    @Override
+    public void ordinaryFoo() {
+        System.out.println("A free realization");
+    }
+
+    @Override
+    public void defaultFoo2() {
+        System.out.println("Not necessary, actually");
+    }
+}
+
+
